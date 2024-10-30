325d178e42cc51401f463ed99252a6b53fb3b292
Miguel Valdes <miguel.alfva@hotmail.com>
2024-10-29 22:24:41 -0600

    Se agrega lista de requerimientos

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Chromedriver downloader
+# Chromedriver downloader :computer:
+
+Descarga la última versión estable del chromedriver, util por su se usa selenium
+
+## Modo de uso :bookmark_tabs:
+
+1. Instalar requerimientos
+```
+pip install -r requirements.txt
+```
+
+2. Correr el script
+```
+python proceso.py
+```
+
+Y asi de fácil se obtiene la última versión del driver actual
--- proceso.py
+++ proceso.py
@@ -5,25 +5,29 @@ import requests, shutil, os
 
 URL = 'https://googlechromelabs.github.io/chrome-for-testing/'
 
-print('Inicia proceso de descarga de Chromedriver')
+def proceso() :
+    print('Inicia proceso de descarga de Chromedriver')
 
-request = requests.get(URL)
-soup = BeautifulSoup(request.text, 'html.parser')
-dom = etree.HTML(str(soup))
+    request = requests.get(URL)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    dom = etree.HTML(str(soup))
 
-node = dom.xpath('//h2[.="Stable"]/following-sibling::div[@class="table-wrapper"]/table//th[.="chromedriver"]/following-sibling::th[.="win64"]/following-sibling::td/code')
+    node = dom.xpath('//h2[.="Stable"]/following-sibling::div[@class="table-wrapper"]/table//th[.="chromedriver"]/following-sibling::th[.="win64"]/following-sibling::td/code')
 
-os.makedirs('tmp')
-with open('./tmp/chromedriver.zip', "wb") as file :
-    request = requests.get(node[0].text.strip(), stream=True)
-    for chunk in request.iter_content(chunk_size=128):
-        file.write(chunk)
+    os.makedirs('tmp')
+    with open('./tmp/chromedriver.zip', "wb") as file :
+        request = requests.get(node[0].text.strip(), stream=True)
+        for chunk in request.iter_content(chunk_size=128):
+            file.write(chunk)
 
-input_zip = ZipFile('./tmp/chromedriver.zip')
-input_zip.extractall('./tmp')
-input_zip.close()
+    input_zip = ZipFile('./tmp/chromedriver.zip')
+    input_zip.extractall('./tmp')
+    input_zip.close()
 
-shutil.move('./tmp/chromedriver-win64/chromedriver.exe', './chromedriver.exe')
-shutil.rmtree('./tmp')
+    shutil.move('./tmp/chromedriver-win64/chromedriver.exe', './chromedriver.exe')
+    shutil.rmtree('./tmp')
 
-print('Fin de proceso de descarga de Chromedriver')
+    print('Fin de proceso de descarga de Chromedriver')
+
+if __name__ == '__main__' :
+    proceso()
